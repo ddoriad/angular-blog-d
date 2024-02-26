9b32f5dbfe9a8c69648b55eb37d21e74981891da
ddoriad <123431777+ddoriad@users.noreply.github.com>
2024-02-25 22:43:54 -0300

    Update dataFake.ts

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "NFinal Fantasy VII Rebirth",
+    "description": "Final Fantasy VII Rebirth chega ao PS5. Pronto para continuar a saga de Cloud?",
+    "photoCover":"https://meups.com.br/wp-content/uploads/2024/02/Final-Fantasy-VII-Rebirth-14-900x503.jpg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "Esquadrão Suicida: Mate a Liga da Justiça",
+    "description": "Esquadrão Suicida: Mate a Liga da Justiça traz a equipe de vilões lutando contra os icônicos super-heróis da DC",
+    "photoCover":"https://www.digitaltrends.com/wp-content/uploads/2023/11/Suicide-Squad-Kill-the-justice-League-key-art.jpg?fit=720%2C405&p=1"
   }
 ]
